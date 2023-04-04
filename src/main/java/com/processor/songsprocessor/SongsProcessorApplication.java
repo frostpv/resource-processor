4e5ab53ccd7f11920bed3316d2b7c0cf736f5358
vasilijpetecuk <frostpv@gmail.com>
2023-04-04 20:39:44 +0300

    first commit

---
+++ src/main/java/com/processor/songsprocessor/SongsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.processor.songsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsProcessorApplication.class, args);
+	}
+
+}
